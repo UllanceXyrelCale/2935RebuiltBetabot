c86e6991396528da61f5e9f159ee5d5608967898
Ullance Xyrel Cale <uxjcale710@gmail.com>
2026-02-20 15:51:41 -0500

    intial commit

--- src/main/java/frc/robot/subsystems/FloorSubsystem.java
+++ src/main/java/frc/robot/subsystems/FloorSubsystem.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
 
